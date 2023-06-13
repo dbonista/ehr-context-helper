0ef9d87d507cad5ed741bb523236d8f122b9b8d0
Daniel Bonistalli <ia0341@USIT0240.local>
2023-06-13 11:31:15 -0400

    put the enc ID on the thirdline

--- context.js
+++ context.js
@@ -3,8 +3,12 @@ if (window.name == 'frMain') { //only load if we're in the clinical frame in Ath
 	//get the encounter briefing element
 	const briefing = document.getElementsByClassName("encounter-stamp");
 
+	//get the third line element
+	const thirdline = document.getElementsByClassName("horizontal banner-third-line");
+
 	//get data that's saved on the page meta data
 	var target = {"encounter_id": ""}; //set encounter_id as blank by default
+
 	//override NA encounter with page, if applicable
 	if (document.getElementById("default-data-context")){
 		const ary = JSON.parse(document.getElementById("default-data-context").content);
@@ -12,10 +16,19 @@ if (window.name == 'frMain') { //only load if we're in the clinical frame in Ath
 			Object.assign(target, ary[i]);
 		}
 	}
+
+	// //set patient header(s) with new encounter element
+	// Array.prototype.forEach.call(briefing, element => {
+	// 	enc_elem = document.createElement("span")
+	// 	enc_elem.textContent = "ENC#" + target.encounter_id;
+	// 	element.prepend(enc_elem);
+	// })
+
 	//set patient header(s) with new encounter element
-	Array.prototype.forEach.call(briefing, element => {
-		enc_elem = document.createElement("span")
+	Array.prototype.forEach.call(thirdline, element => {
+		enc_elem = document.createElement("li")
 		enc_elem.textContent = "ENC#" + target.encounter_id;
+		enc_elem.style.color = "white";
 		element.prepend(enc_elem);
 	})
 }
